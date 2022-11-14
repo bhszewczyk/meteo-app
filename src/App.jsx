d07d3221bfda4b1da3eff9d0acb51765cf12db9e
Barbara Szewczyk <comamysza@gmail.com>
2022-11-15 00:08:00 +0100

    Adds starting files, fetches data from local server

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+	const [reportData, setReportData] = useState([]);
+
+	useEffect(() => {
+		fetchReports();
+	}, []);
+
+	function fetchReports() {
+		fetch('http://localhost:8000/api/reports').then((res) =>
+			res.json().then((data) => {
+				setReportData(...data);
+			})
+		);
+	}
+
+	console.log(reportData);
+
+	return <div className='App'></div>;
+}
+
+export default App;
